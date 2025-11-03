cbc980a9e4aaf821c9be6fe0c45094e602e774fa
Raka Muhammad <Raka>
2025-11-03 10:59:45 +0700

    Add sort for skill

--- routes/skill.ts
+++ routes/skill.ts
@@ -2,12 +2,12 @@ import Skill from '../models/skill'
 
 export const getSkills = async (ctx: any) => {
   try {
-    const skills = await Skill.find()
+    const skills = await Skill.find().sort({ _id: 1 })
 
     ctx.status = 200
     ctx.body = { skills }
   } catch (error) {
-    console.error('Error fetching projects:', error)
+    console.error('Error fetching skills:', error)
 
     ctx.status = 500
     ctx.body = { error: 'Internal Server Error' }
